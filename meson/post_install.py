fa8909da0aa698a679ba18f36eb7abcb173c2d82
Jeremy Wootten <jeremy@elementaryos.org>
2018-01-17 17:36:26 +0000

    Update mimetype database after install

--- meson/post_install.py
+++ meson/post_install.py
@@ -4,7 +4,10 @@ import os
 import subprocess
 
 schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+mimetypedir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'mime')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir])
+    print('Updating mimetype database...')
+    subprocess.call(['update-mime-database', mimetypedir])
